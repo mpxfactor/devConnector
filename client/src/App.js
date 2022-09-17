ff36d657931a9724cb4e22fd18810f3480bd5b09
mpxfactor <tancmpsinc@gmail.com>
2022-09-17 07:26:53 +0530

    sep_17_07:26

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import { Fragment } from "react";
-import { Navbar } from "./components/Navbar";
-import { Landing } from "./components/Landing";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/layout/Navbar";
+import { Landing } from "./components/layout/Landing";
+import { Login } from "./components/auth/Login";
+import { Register } from "./components/auth/Register";
+
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 const App = () => {
   return (
-    <Fragment>
-      <Navbar/>
-      <Landing/>
-    </Fragment>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
